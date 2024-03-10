475b64d1c5a90fe671e5df0cfe03a71b22a6dca9
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 11:00:19 +0500

    Add files via upload

---
+++ 29 favorite_fruit.ts
@@ -0,0 +1,37 @@
+// favorite fruits in the array
+
+let fruits: string[] = ["apple", "mango", "banana"];
+
+if (fruits.includes("apple")) {
+    console.log("you have a apple in your array");
+}
+if (fruits.includes("banana")) {
+    console.log("you have a banana in your array");
+}
+if (fruits.includes("mango")) {
+    console.log("you have a mango in your array");
+}
+else {
+    console.log("you dont have an orange your array");
+}
+
+
+// my favorite fruits
+
+let favorite_fruits: string[] = ['grapes', 'olive', 'apple'];
+
+if (favorite_fruits.includes("grapes")) {
+    console.log('you really like grapes!');
+}
+if (favorite_fruits.includes("olive")) {
+    console.log('you really like olive!');
+}
+if (favorite_fruits.includes("apple")) {
+    console.log('you really like apple!');
+}
+if (favorite_fruits.includes("banana")) {
+    console.log('you really like apple!');
+}
+if (favorite_fruits.includes("orange")) {
+    console.log('you really like apple!');
+}
