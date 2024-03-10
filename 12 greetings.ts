32768d8422025665b539acadf640ce2974c3f0e7
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 10:26:45 +0500

    Add files via upload

---
+++ 12 greetings.ts
@@ -0,0 +1,9 @@
+//list of array
+
+let friends: string[] = ['ali', 'murtaza', 'ameer', 'mustafa'];
+friends.forEach(friend => {
+    console.log(`hello ${friend} would you like to learn some python today?.`);
+
+});
+    
+
