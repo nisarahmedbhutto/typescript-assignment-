170e2537f106d4cd3f18d948d631cc7a511c61fe
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 10:58:13 +0500

    Add files via upload

---
+++ 27 alian_color_#3.ts
@@ -0,0 +1,49 @@
+// alian color #3
+
+// version 1
+
+let alianColors: string = "green";
+
+// alian if condition with console 
+
+if (alianColors == "green") {
+    console.log("player just earn 5 points");
+} else if (alianColors == "yellow") {
+    console.log("player just earn 10 points");
+} else if (alianColors == "red") {
+    console.log("player just earn 15 points");
+}
+
+
+
+// version 2
+
+alianColors = "yellow";
+
+// alian if condition with console
+
+if (alianColors == "green") {
+    console.log("player just earn 5 points");
+} else if (alianColors == "yellow") {
+    console.log("player just earn 10 points");
+} else if (alianColors == "red") {
+    console.log("player just earn 15 points");
+}
+
+
+// version 3
+
+alianColors = "red";
+
+// alian if condition with console
+
+if (alianColors == "green") {
+    console.log("player just earn 5 points");
+} else if (alianColors == "yellow") {
+    console.log("player just earn 10 points");
+} else if (alianColors == "red") {
+    console.log("player just earn 15 points");
+}
+
+
+
