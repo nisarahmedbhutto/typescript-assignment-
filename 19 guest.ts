4a04107531d952dac51810a51c7de424fc69d21b
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 10:43:34 +0500

    Add files via upload

---
+++ 19 guest.ts
@@ -0,0 +1,12 @@
+// 14 exersise guest list for dinner
+
+let guestList: string[] = ["imran khan", "asif ali zardari", "nawaz shareef"];
+let message: string = "respected sir, i invite you to dinner tonight.";
+ 
+//invitetion for all guest
+guestList.forEach(guestList => {
+    console.log(guestList +" "+ message);
+});
+
+// 19 exercise dinner guest list
+console.log("i am inviting 3 people to dinner.")
