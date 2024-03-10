92fa556799b3d693545ba4c909d98ea5877758ba
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 11:27:59 +0500

    Add files via upload

---
+++ 21 they_think_of_somthing_you_could_store_in_a_typescript _object.ts
@@ -0,0 +1,8 @@
+//object 
+let country = {
+    countryName: "pakistan",
+    creator: "muhammad ali jinah",
+    location: "best location in the world",
+    population: "the apporximate population of pakistan is now around 240 million"
+}
+console.log(country);
