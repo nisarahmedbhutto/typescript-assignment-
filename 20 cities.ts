f941e8e9b43ff33409635739d0de4fecfa330e58
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 11:21:53 +0500

    Delete 20 cities.ts

--- 20 cities.ts
+++
@@ -1,8 +0,0 @@
-let citeis: string[] = ['newyork', 'tokyo', 'london', 'karachi'];
-console.log('citeis');
-//citeis.forEach(citeis => {
-  //  console.log(citeis);
-    //})
-for (var i = 0; i < citeis.length; i++){
-    console.log(citeis[i]);
-}
