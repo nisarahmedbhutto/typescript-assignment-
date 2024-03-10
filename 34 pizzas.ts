187339d12ac0518e2e29d479bc135c31946785ec
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 11:06:02 +0500

    Add files via upload

---
+++ 34 pizzas.ts
@@ -0,0 +1,7 @@
+// pizzas array
+
+let pizzas: string[] = ['veggie', 'margherita', 'pepperoni'];
+for (let i = 0; i < pizzas.length; i++){
+    console.log(`i like ${pizzas[i]} pizza`);
+}
+console.log("i real love pizzas");
