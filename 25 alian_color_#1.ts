e2db845ac77a6a058fadaf62523eb0798fa71a51
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 10:56:00 +0500

    Add files via upload

---
+++ 25 alian_color_#1.ts
@@ -0,0 +1,13 @@
+// alian colors #1
+
+let alianColor: string = "green";
+
+// version 1 passed
+if (alianColor == "green") {
+    console.log("player just earn 5 points.")
+}
+
+// version 2 failed
+if (alianColor == "red") {
+    console.log("player just earn 5 points.")
+}
