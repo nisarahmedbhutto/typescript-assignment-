31649a80f90c270008619929fc175c0c61c19e52
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 10:49:05 +0500

    Add files via upload

---
+++ 22 international_error.ts
@@ -0,0 +1,15 @@
+// own a transports
+
+let favorute_transport: string[] = ['car', 'cycle', 'honda', 'buss'];
+
+
+//loop and console print
+favorute_transport.forEach(favorute_transport => {
+    console.log(`i would like to own a ${favorute_transport}`);
+});
+
+console.log(favorute_transport[5]);
+
+// again console and print
+console.log(favorute_transport[3]);
+
