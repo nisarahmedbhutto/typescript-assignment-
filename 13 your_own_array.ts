f7a8cc924348a88939915cd9619e53fac04b01fe
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 10:28:04 +0500

    Add files via upload

---
+++ 13 your_own_array.ts
@@ -0,0 +1,8 @@
+// own a transports
+
+let favorute_transport: string[] = ['car', 'cycle', 'honda', 'buss'];
+
+//loop and console print
+favorute_transport.forEach(favorute_transport => {
+    console.log(`i would like to own a ${favorute_transport}`);
+});
