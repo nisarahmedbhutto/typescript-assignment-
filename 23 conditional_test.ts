d7ab1403dac50906b9ad8a10979a4333c487b216
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 10:50:43 +0500

    Add files via upload

---
+++ 23 conditional_test.ts
@@ -0,0 +1,68 @@
+// conditional test
+
+let fruite: string = "apple";
+let bike: string = "honda";
+
+// this is a 5 conditions with true
+// 1st test
+console.log("is fruite == 'apple'? i predict true.")
+console.log(fruite == "apple");
+
+// 2nd test
+console.log("is fruite === 'apple'? i predict true.")
+console.log(fruite === "apple");
+
+// 3rd test
+console.log("is fruite != 'mango'? i predict true.")
+console.log(fruite != "mango");
+
+// 4th test
+console.log("is fruite !== 'mango'? i predict true.")
+console.log(fruite !== "mango");
+
+// 5th test
+console.log("is fruite !== 'apple'? i predict true.")
+console.log(fruite.toUpperCase() !== "apple");
+
+
+
+// this is a 5 conditions with false
+// 6th test
+console.log("is bike == 'honda 125'? i predict true.")
+console.log(bike == "honda 125");
+
+// 7th test
+console.log("is bike === 'honda 125'? i predict true.")
+console.log(bike === "honda 125");
+
+// 8th test
+console.log("is bike !== 'honda'? i predict true.")
+console.log(bike !== "honda");
+
+// 9th test
+console.log("is bike != 'honda'? i predict true.")
+console.log(bike != "honda");
+
+// 10th test
+console.log("is bike == 'car'? i predict true.")
+console.log(bike == "car");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
