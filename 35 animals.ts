f0ad90227b23f1dc66f958e85578604a179a13f7
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 11:08:15 +0500

    Add files via upload

---
+++ 35 animals.ts
@@ -0,0 +1,16 @@
+// animals array
+
+let animals: string[] = ["dog", "cat", "rabbit"];
+
+// for loop print the reasult
+for (let o = 0; o < animals.length; o++){
+    if (animals == animals) {
+        console.log(`a ${animals[o]} would make a great pet.`);
+    } else if (animals == animals) {
+        console.log(`a ${animals[o]} is an independent companain.`);
+    } else if (animals == animals) {
+        console.log(`a ${animals[o]} can be a playful addition to your home.`);
+    }
+
+}
+console.log('any of these animals would make a great pet');
