3671d2af3919a5e4bda586af1a577c5b09614556
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 11:10:08 +0500

    Add files via upload

---
+++ 37 large_shirts.ts
@@ -0,0 +1,10 @@
+// large shirts
+
+function make_shirts(size:string="large",message:string="i love typescript") {
+    console.log(`my shirt size is ${size} with message ${message} is ready.`);
+
+}
+// function callings
+make_shirts("large",);
+make_shirts("medium");
+make_shirts("small","game start now")
