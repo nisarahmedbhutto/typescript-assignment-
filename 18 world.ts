be68fc3542e0cabb02ec169ed292834b283bb29b
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 10:42:26 +0500

    Add files via upload

---
+++ 18 world.ts
@@ -0,0 +1,12 @@
+// seeing in the world
+
+let pleces: string[] = ["paris", "dubai", "maree", "switzerland", "montevede"];
+console.log(pleces);
+console.log([...pleces].sort());
+console.log(pleces);
+console.log([...pleces].sort().reverse());
+console.log(pleces);
+console.log(pleces.reverse());
+console.log(pleces.reverse());
+console.log(pleces.reverse().sort());
+console.log(pleces.sort().reverse());
