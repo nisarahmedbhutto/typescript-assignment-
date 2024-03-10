92e4a7d201f4a7ebb22c34920bdf6acd68a1ebe3
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 10:17:31 +0500

    Add files via upload

---
+++ 5 famous_quote_2.ts
@@ -0,0 +1,9 @@
+//famous quote 2
+
+//typing the variables
+
+let personName: string = 'tipu sultan';
+let message: string = "one day of a lion's life is better than a hundred years of a jackal's life."
+
+//print the console 
+console.log(`${personName} ${message}`);
