281ad838f948470460345e717e9ef9618c157677
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 10:45:29 +0500

    Add files via upload

---
+++ 20 cities.ts
@@ -0,0 +1,8 @@
+let citeis: string[] = ['newyork', 'tokyo', 'london', 'karachi'];
+console.log('citeis');
+//citeis.forEach(citeis => {
+  //  console.log(citeis);
+    //})
+for (var i = 0; i < citeis.length; i++){
+    console.log(citeis[i]);
+}
