8c31a055e8384e1cae6c7f33cf6921e288226356
nisarahmedbhutto <159589661+nisarahmedbhutto@users.noreply.github.com>
2024-03-10 11:25:40 +0500

    Delete 21 object.ts

--- 21 object.ts
+++
@@ -1,8 +0,0 @@
-//object 
-let country = {
-    countryName: "pakistan",
-    creator: "muhammad ali jinah",
-    location: "best location in the world",
-    population: "the apporximate population of pakistan is now around 240 million"
-}
-console.log(country);
